6c5c9d9e4c547874a2c6c506b26a038f4fc4f46f
Fatih Ersoy <f.ersoytx@gmail.com>
2019-11-17 13:38:30 -0600

    models and assosiations has been added

--- v1/app.js
+++ v1/app.js
@@ -2,17 +2,16 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var seedDB = require("./db/seed");
+
+seedDB();
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", { useUnifiedTopology: true, useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-var Campground = mongoose.model("Campground", campgroundSchema);
+
 
 // Campground.create(
 //     {
