1ad273055351de0bc178c27e7c3f4c3b199a2a07
Fatih Ersoy <f.ersoytx@gmail.com>
2019-11-18 21:25:33 -0600

    camment/new page was created but gives an error

--- v1/app.js
+++ v1/app.js
@@ -3,7 +3,8 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
-var seedDB = require("./models/seed");
+var Comment = require("./models/comment");
+var seedDB = require("./seed");
 
 seedDB();
 
@@ -22,7 +23,7 @@ app.get("/campgrounds", function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("index", { campgrounds: allcampgrounds })
+            res.render("campgrounds/index", { campgrounds: allcampgrounds })
 
 
         }
@@ -46,7 +47,7 @@ app.post('/campgrounds', function (req, res) {
 
 });
 app.get("/campgrounds/new", function (req, res) {
-    res.render("new.ejs");
+    res.render("campgrounds/new");
 });
 
 app.get('/campgrounds/:id', function (req, res) {
@@ -54,10 +55,40 @@ app.get('/campgrounds/:id', function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("show", { Campground: receivedId });
+            res.render("campgrounds/show", { Campground: receivedId });
         }
     })
 });
+
+app.get("/campgrounds/:id/comments/new", function (req, res) {
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render("comments/new", { campground: campground });
+        }
+    })
+});
+app.post(("/campgrounds/:id/comments", function (req, res) {
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function (err, comment) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect('/campgrounds/' + campground._id);
+                };
+            });
+        };
+    });
+}));
+
+
 app.listen(3000, function () {
     console.log("The yelp camp server has started!");
 });
